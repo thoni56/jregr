a9ce3be6dee170f0726ef573b7a773f96258ea3c
Thomas Nilefalk <thomas@junovagen.se>
2018-02-03 21:48:48 +0100

    Add description of configuration file

--- README.md
+++ README.md
@@ -13,9 +13,7 @@ Synopsis
 
 In a configuration file per directory, `.jregr`, you specify a pattern
 for what is considered a test case, and the command(s) that should be
-performed on each of the test cases. For example
-
-    .sh: sh $1.sh
+performed on each of the test cases.
 
 The output (meaning everything that is sent to the standard output) is
 considered the actual output of the test case. This is compared to the
@@ -27,18 +25,39 @@ If they are identical the test case passed, if not it failed.
 Format of configuration file
 ----------------------------
 
-Possibly, this is the sinplest configuration file imaginable. It should
-be read like:
+Possibly, the following is the simplest configuration file
+imaginable:
+
+    .sh: sh $1.sh
+
+It should be read like:
 
 -   each test case is a file ending in `.sh`
 -   for each such file in this directory: run `sh` on it
 
-So the pattern is
+The pattern is
 
-    <extension> ':' <command line>
+    <extension> ':' <command> [ '<' <input> ]
 
-It is possible to have multiple lines, which will be run in sequence.
+If the `<` is present, Jregr will try to find a file as specified
+after it and use that as the standard input to the `<command>`.  It is
+possible to have multiple lines, which will be run in sequence, with
+each line having an <extension>, a <command> and an optional <input>.
+
 The actual output will be the total output of running all lines.
+
+
+Configuration File Variables
+----------------------------
+
+There are some 'variables' available for use in the configuration
+file:
+
+`$1` - the case name, which is always the basename of the file matching
+the extension on the first line in the configuration file
+
+`$2` - the complete file name for the file matching the extension on
+the current line
 
 Running
 -------
@@ -46,16 +65,19 @@ Running
 Navigate to a directory containing a `.jregr` configuration file and run
 `jregr`. With no options or arguments, `jregr` will run all test cases
 and report the progress on the standard output. Passing tests will
-disapear from view, while failing, suspended and ignored test cases will
-remain. Finally `jregr` will give a summary.
+disappear from view, while failing, suspended and ignored test cases
+will remain. Finally `jregr` will give a summary.
 
 Options
 -------
 
-`-bin`		find any executable files in this directory
+`-bin` find any executable files in this directory
 
-`-dir`		find testcases and expected output in this directory, also write actual output there
+`-dir` find testcases and expected output in this directory, also write
+actual output there
 
-`-xml`		create Junit compatible `xml`files instead of output to console (collect in Jenkins etc.)
+`-xml` create Junit compatible `xml`files instead of output to console
+(collect in Jenkins etc.)
 
-`-noansi`	don't use ansi codes to erase passing tests from console output, so will show every test case as it runs
+`-noansi` don't use ansi codes to erase passing tests from console
+output, so will show every test case as it runs
--- src/se/alanif/jregr/CommandsDecoder.java
+++ src/se/alanif/jregr/CommandsDecoder.java
@@ -8,150 +8,151 @@ import se.alanif.jregr.io.Directory;
 
 public class CommandsDecoder {
 
-	// The RegrDecoder decodes a file (presumably a .jregr file) into
-	// <extension> ':' <command> {<arg>} [ '<' <stdin> ]
-	// It will expand $1 to the case name and $2 to the full filename matched by the line (case name + extension)
-	
-	// It will work on one line at a time starting with the first
-	// Use advance() to advance to next line
-	// Feed it a BufferedReader in the constructor
+    // The RegrDecoder decodes a file (presumably a .jregr file) into
+    // <extension> ':' <command> {<arg>} [ '<' <stdin> ]
+    // It will expand $1 to the case name and $2 to the full filename
+    // matched by the line (case name + extension)
 
-	// It also handles the case where there is no .jregr file
-	// and then serves the standard commands ".alan : alan $1" + ".a3c : arun $1 < $1.input"
+    // It will work on one line at a time starting with the first
+    // Use advance() to advance to next line
+    // Feed it a BufferedReader in the constructor
 
-	private static final String[] DEFAULT_EXTENSION =   new String[]   {".alan",            ".input"};
-	private static final String[] DEFAULT_COMMAND =     new String[]   {"alan",             "arun"};
-	private static final String[][] DEFAULT_ARGUMENTS = new String[][] {new String[]{"$1"}, new String[]{"-n", "-r", "$1"}};
-	private static final String[] STDIN =               new String[]   {null,               "$1.input"};
+    // It also handles the case where there is no .jregr file
+    // and then serves the standard commands ".alan : alan $1" + ".a3c : arun $1 < $1.input"
 
-	private String[] words;
-	private BufferedReader jregrFileReader;
-	private boolean jregrFileExists;
-	private int defaultSet = 0;
-	private String stdin;
+    private static final String[] DEFAULT_EXTENSION =   new String[]   {".alan",            ".input"};
+    private static final String[] DEFAULT_COMMAND =     new String[]   {"alan",             "arun"};
+    private static final String[][] DEFAULT_ARGUMENTS = new String[][] {new String[]{"$1"}, new String[]{"-n", "-r", "$1"}};
+    private static final String[] STDIN =               new String[]   {null,               "$1.input"};
 
-	public CommandsDecoder(BufferedReader fileReader) {
-		if (fileReader == null)
-			jregrFileExists = false;
-		else {
-			jregrFileExists = true;
-			jregrFileReader = fileReader;
-			try {
-				fileReader.mark(10000);
-				words = splitIntoWords(this.jregrFileReader.readLine());
-			} catch (IOException e) {
-				jregrFileExists = false;
-			}
-		}
-	}
+    private String[] words;
+    private BufferedReader jregrFileReader;
+    private boolean jregrFileExists;
+    private int defaultSet = 0;
+    private String stdin;
 
-	private String[] splitIntoWords(String line) throws IOException {
-		String[] split = line.split(" ");
-		split = decodeStdin(split);
-		return removeColonInSecondPosition(split);
-	}
+    public CommandsDecoder(BufferedReader fileReader) {
+        if (fileReader == null)
+            jregrFileExists = false;
+        else {
+            jregrFileExists = true;
+            jregrFileReader = fileReader;
+            try {
+                fileReader.mark(10000);
+                words = splitIntoWords(this.jregrFileReader.readLine());
+            } catch (IOException e) {
+                jregrFileExists = false;
+            }
+        }
+    }
 
-	private String expandSymbols(String caseName, String argument) {
-		argument = argument.replaceAll("\\$1", caseName);
-		argument = argument.replaceAll("\\$2", caseName+getExtension());
-		return argument;
-	}
+    private String[] splitIntoWords(String line) throws IOException {
+        String[] split = line.split(" ");
+        split = decodeStdin(split);
+        return removeColonInSecondPosition(split);
+    }
 
-	private String[] decodeStdin(String[] split) {
-		if (split[split.length-2].equals("<")) {
-			stdin = split[split.length-1];
-			split = Arrays.copyOf(split, split.length-2);
-		} else
-			stdin = null;
-		return split;
-	}
+    private String expandSymbols(String caseName, String argument) {
+        argument = argument.replaceAll("\\$1", caseName);
+        argument = argument.replaceAll("\\$2", caseName+getExtension());
+        return argument;
+    }
 
-	private String[] removeColonInSecondPosition(String[] split) {
-		String[] w = Arrays.copyOf(split, split.length-1);
-		for (int i=1; i<w.length; i++)
-			w[i] = split[i+1];
-		return w;
-	}
+    private String[] decodeStdin(String[] split) {
+        if (split[split.length-2].equals("<")) {
+            stdin = split[split.length-1];
+            split = Arrays.copyOf(split, split.length-2);
+        } else
+            stdin = null;
+        return split;
+    }
 
-	public String getCommand() {
-		if (jregrFileExists)
-			return words[1];
-		else
-			return DEFAULT_COMMAND[defaultSet];
-	}
+    private String[] removeColonInSecondPosition(String[] split) {
+        String[] w = Arrays.copyOf(split, split.length-1);
+        for (int i=1; i<w.length; i++)
+            w[i] = split[i+1];
+        return w;
+    }
 
-	private String[] getArguments() {
-		if (jregrFileExists) {
-			String [] arguments = new String[words.length - 2];
-			for (int i = 2; i < words.length; i++)
-				arguments[i-2] = words[i];
-			return arguments;
-		} else
-			return DEFAULT_ARGUMENTS[defaultSet];
-	}
+    public String getCommand() {
+        if (jregrFileExists)
+            return words[1];
+        else
+            return DEFAULT_COMMAND[defaultSet];
+    }
 
-	public String getExtension() {
-		if (jregrFileExists)
-			return words[0];
-		else {
-			return DEFAULT_EXTENSION[defaultSet];
-		}
-	}
+    private String[] getArguments() {
+        if (jregrFileExists) {
+            String [] arguments = new String[words.length - 2];
+            for (int i = 2; i < words.length; i++)
+                arguments[i-2] = words[i];
+            return arguments;
+        } else
+            return DEFAULT_ARGUMENTS[defaultSet];
+    }
 
-	public String getStdin(String caseName) {
-		String r;
-		if (!jregrFileExists)
-			r = STDIN[defaultSet];
-		else
-			r = stdin;
-		if (r != null)
-			return expandSymbols(caseName, r);
-		else
-			return r;
-	}
+    public String getExtension() {
+        if (jregrFileExists)
+            return words[0];
+        else {
+            return DEFAULT_EXTENSION[defaultSet];
+        }
+    }
 
-	public String[] buildCommandAndArguments(Directory binDirectory, String caseName) {
-		final String binPath = binDirectory != null? binDirectory.getAbsolutePath() + Directory.separator : "";
-		String command;
-		if (binDirectory == null || !binDirectory.executableExist(getCommand()))
-			command = expandSymbols(caseName, getCommand());
-		else
-			command = binPath + expandSymbols(caseName, getCommand());
-		String[] arguments = getArguments();
-		String[] commandAndArguments = new String[arguments.length + 1];
-		commandAndArguments[0] = command;
-		for (int i = 0; i < arguments.length; i++) {
-			String argument = arguments[i];
-			argument = expandSymbols(caseName, argument);
-			commandAndArguments[i + 1] = argument;
-		}
-		return commandAndArguments;
-	}
+    public String getStdin(String caseName) {
+        String r;
+        if (!jregrFileExists)
+            r = STDIN[defaultSet];
+        else
+            r = stdin;
+        if (r != null)
+            return expandSymbols(caseName, r);
+        else
+            return r;
+    }
 
-	public boolean advance() {
-		if (jregrFileExists) {
-			try {
-				final String line = jregrFileReader.readLine();
-				if (line == null || line == "") return false;
-				words = splitIntoWords(line);
-				return true;
-			} catch (IOException e) {
-				return false;
-			}
-		} else
-			return defaultSet++ < 1;
-	}
+    public String[] buildCommandAndArguments(Directory binDirectory, String caseName) {
+        final String binPath = binDirectory != null? binDirectory.getAbsolutePath() + Directory.separator : "";
+        String command;
+        if (binDirectory == null || !binDirectory.executableExist(getCommand()))
+            command = expandSymbols(caseName, getCommand());
+        else
+            command = binPath + expandSymbols(caseName, getCommand());
+        String[] arguments = getArguments();
+        String[] commandAndArguments = new String[arguments.length + 1];
+        commandAndArguments[0] = command;
+        for (int i = 0; i < arguments.length; i++) {
+            String argument = arguments[i];
+            argument = expandSymbols(caseName, argument);
+            commandAndArguments[i + 1] = argument;
+        }
+        return commandAndArguments;
+    }
 
-	public void reset() {
-		if (!jregrFileExists)
-			defaultSet = 0;
-		else
-			try {
-				jregrFileReader.reset();
-				words = splitIntoWords(this.jregrFileReader.readLine());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	}
+    public boolean advance() {
+        if (jregrFileExists) {
+            try {
+                final String line = jregrFileReader.readLine();
+                if (line == null || line == "") return false;
+                words = splitIntoWords(line);
+                return true;
+            } catch (IOException e) {
+                return false;
+            }
+        } else
+            return defaultSet++ < 1;
+    }
+
+    public void reset() {
+        if (!jregrFileExists)
+            defaultSet = 0;
+        else
+            try {
+                jregrFileReader.reset();
+                words = splitIntoWords(this.jregrFileReader.readLine());
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+    }
 }
