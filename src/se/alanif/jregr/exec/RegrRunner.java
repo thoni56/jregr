00e382639ca84e7194ce91cfe76119acd4882916
Thomas Nilefalk <thomas@nilefalk.se>
2018-11-03 09:32:55 +0100

    Attempt to use char encoding on output

--- src/se/alanif/jregr/exec/RegrRunner.java
+++ src/se/alanif/jregr/exec/RegrRunner.java
@@ -2,6 +2,7 @@ package se.alanif.jregr.exec;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 import org.apache.commons.cli.CommandLine;
 
@@ -11,21 +12,21 @@ import se.alanif.jregr.reporters.RegrReporter;
 
 public class RegrRunner {
 
-	public void runCases(RegrCase[] cases, RegrReporter reporter, Directory bindir, String suiteName, CommandsDecoder decoder, CommandLine commandLine) throws FileNotFoundException {
-		reporter.start(suiteName, cases.length, commandLine);
-		for (RegrCase theCase : cases) {
-			PrintWriter printWriter = new PrintWriter(theCase.getOutputFile());
-			decoder.reset();
-			long start = System.currentTimeMillis();
-			theCase.run(bindir, decoder, printWriter, new CaseRunner(), new ProcessBuilder());
-			long end = System.currentTimeMillis();
-			theCase.clean();
+    public void runCases(RegrCase[] cases, RegrReporter reporter, Directory bindir, String suiteName, CommandsDecoder decoder, CommandLine commandLine) throws FileNotFoundException {
+        reporter.start(suiteName, cases.length, commandLine);
+        for (RegrCase theCase : cases) {
+            PrintWriter printWriter = new PrintWriter(theCase.getOutputFile(), StandardCharsets.ISO_8859_1);
+            decoder.reset();
+            long start = System.currentTimeMillis();
+            theCase.run(bindir, decoder, printWriter, new CaseRunner(), new ProcessBuilder());
+            long end = System.currentTimeMillis();
+            theCase.clean();
 
-			// TODO Until we use an XML framework we can't call starting() before the test because of the timing info
-			reporter.starting(theCase, end-start);
-			reporter.report(theCase.status());
-		}
-		reporter.end();
-	}
+            // TODO Until we use an XML framework we can't call starting() before the test because of the timing info
+            reporter.starting(theCase, end-start);
+            reporter.report(theCase.status());
+        }
+        reporter.end();
+    }
 
 }
