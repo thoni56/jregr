bb1bc3ef3414e6e0bc1481b360f3b6839ceb7db8
Thomas Nilefalk <thomas@junovagen.se>
2018-11-03 18:53:01 +0100

    Revert "Specify input char encoding to ISO-8859-1"
    We should not specify file encoding inside Jregr. This can be done
    using a JVM define such as:
    
    java -Dfile.encoding=iso-8859-1 ...
    
    And this solves the year long search for problems on Linux (UTF-8, by
    default)
    
    This reverts commit 757b8e86f3a2367610a43cd694f99d92d3676cf6.

--- src/se/alanif/jregr/io/File.java
+++ src/se/alanif/jregr/io/File.java
@@ -1,10 +1,8 @@
 package se.alanif.jregr.io;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 
 public class File extends java.io.File {
 
@@ -18,9 +16,7 @@ public class File extends java.io.File {
 		String content = "";
 
 		try {
-			BufferedReader input = new BufferedReader(
-										new InputStreamReader(
-												new FileInputStream(this), StandardCharsets.ISO_8859_1));
+			BufferedReader input = new BufferedReader(new FileReader(this));
 			try {
 				int ch;
 				while ((ch = input.read()) != -1) {
