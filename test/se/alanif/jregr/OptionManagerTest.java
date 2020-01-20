387134589d810f44695f79485ded027caa070559
Thomas Nilefalk <thomas@junovagen.se>
2020-01-20 20:39:25 +0000

    Change from deprecated to DefaultParser()

--- test/se/alanif/jregr/OptionManagerTest.java
+++ test/se/alanif/jregr/OptionManagerTest.java
@@ -3,26 +3,24 @@ package se.alanif.jregr;
 import junit.framework.TestCase;
 import org.junit.Test;
 
-
-import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.GnuParser;
 
 public class OptionManagerTest extends TestCase {
 
 	@Test
 	public void testCanParseBooleanGuiOption() throws Exception {
 		OptionsManager optionManager = new OptionsManager();
-		String[] args = new String[]{"-gui"};
-		CommandLine commandLine = new BasicParser().parse(optionManager, args);
+		String[] args = new String[] { "-gui" };
+		CommandLine commandLine = new DefaultParser().parse(optionManager, args);
 		assertTrue(commandLine.hasOption("gui"));
 	}
-	
+
 	@Test
 	public void testCanParseBinDirectoryOption() throws Exception {
 		OptionsManager optionManager = new OptionsManager();
-		String[] args = new String[]{"-bin=some/dir"};
-		CommandLine commandLine = new GnuParser().parse(optionManager, args);
+		String[] args = new String[] { "-bin=some/dir" };
+		CommandLine commandLine = new DefaultParser().parse(optionManager, args);
 		assertTrue(commandLine.hasOption("bin"));
 		assertEquals("some/dir", commandLine.getOptionValue("bin"));
 	}
