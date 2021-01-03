72503b82be638a86e1a174a0a378df722c67d591
Thomas Nilefalk <thomas@nilefalk.se>
2021-01-03 16:44:35 +0100

    Remove all tests for default commands

--- test/se/alanif/jregr/CommandsDecoderTest.java
+++ test/se/alanif/jregr/CommandsDecoderTest.java
@@ -32,22 +32,10 @@ public class CommandsDecoderTest extends TestCase {
 	private static final String STDIN = "stdin";
 	private static final String[] SECOND_COMMAND_AND_ARGUMENTS = new String[] {BINPATH+File.separator+COMMAND2, ARG21, OPT21, OPTION_VALUE_21};
 	
-	private static final String DEFAULT_EXTENSION1 = ".alan";
-	private static final String DEFAULT_COMMAND1 = "alan";
-	private static final String DEFAULT_ARG1 = CASENAME;
-	private static final String[] DEFAULT_FIRST_COMMAND_AND_ARGUMENTS = new String[] {BINPATH+File.separator+DEFAULT_COMMAND1, DEFAULT_ARG1};
-
-	private static final String DEFAULT_EXTENSION2 = ".input";
-	private static final String DEFAULT_COMMAND2 = "arun";
-	private static final String DEFAULT_ARG2 = CASENAME;
-	private static final String[] DEFAULT_SECOND_COMMAND_AND_ARGUMENTS = new String[] {BINPATH+File.separator+DEFAULT_COMMAND2, "-n", "-r", DEFAULT_ARG2};
-
-
 	private BufferedReader mockedFileReader = mock(BufferedReader.class);
 	private Directory binDirectory = mock(Directory.class);
 	private Directory binDirectoryWithoutExecutables = mock(Directory.class);
 	private CommandsDecoder decoder;
-	private CommandsDecoder defaultDecoder;
 
 	public void setUp() throws Exception {
 		when(mockedFileReader.readLine()).thenReturn(EXTENSION1+" : "+COMMAND1+" "+ARG11+" "+ARG12);
@@ -60,7 +48,6 @@ public class CommandsDecoderTest extends TestCase {
 		when(binDirectoryWithoutExecutables.executableExist(anyString())).thenReturn(false);
 		
 		decoder = new CommandsDecoder(mockedFileReader);
-		defaultDecoder = new CommandsDecoder(null);
 	}
 	
 	@Test
@@ -69,7 +56,7 @@ public class CommandsDecoderTest extends TestCase {
 	}
 
 	@Test
-	public void testShouldBuildCommandAndArgumentsFromFirstLine() throws Exception {
+	public void testShouldBuildCommandAndArgumentsFromCurrentJregrLine() throws Exception {
 		assertTrue(Arrays.equals(FIRST_COMMAND_AND_ARGUMENTS, decoder.buildCommandAndArguments(binDirectory, CASENAME)));
 	}
 	
@@ -107,7 +94,7 @@ public class CommandsDecoderTest extends TestCase {
 	}
 
 	@Test
-	public void testShouldBuildCommandAndArgumentsFromSecondLine() throws Exception {
+	public void testShouldBuildCommandAndArgumentsAfterAdvancingToSecondLine() throws Exception {
 		when(mockedFileReader.readLine()).thenReturn(EXTENSION2+" : "+COMMAND2+" "+ARG21+" "+OPT21+" "+OPTION_VALUE_21);
 		
 		decoder.advance();
@@ -123,45 +110,5 @@ public class CommandsDecoderTest extends TestCase {
 		verify(mockedFileReader).reset();
 		assertTrue(Arrays.equals(FIRST_COMMAND_AND_ARGUMENTS, decoder.buildCommandAndArguments(binDirectory, CASENAME)));
 	}
-
-	@Test
-	public void testShouldReturnDefaultExtensionFirstSetWhenNoJregrFileReader() throws Exception {
-		assertEquals(DEFAULT_EXTENSION1, defaultDecoder.getExtension());
-	}
-	
-	@Test
-	public void testShouldBuildDefaultCommandAndArgumentsWhenNoJregrFile() throws Exception {
-		assertTrue(Arrays.equals(DEFAULT_FIRST_COMMAND_AND_ARGUMENTS, defaultDecoder.buildCommandAndArguments(binDirectory, CASENAME)));
-	}
-	
-	@Test
-	public void testShouldReturnNullAsStdinForFirstSetWhenNoJregrfile() throws Exception {
-		assertEquals(null, defaultDecoder.getStdin(CASENAME));
-	}
-
-	@Test
-	public void testShouldBeAbleToDeliverTwoSetsOfDefaultValuesWhenNoJregrReader() throws Exception {
-		assertTrue(defaultDecoder.advance());
-		assertFalse(defaultDecoder.advance());
-	}
-
-	@Test
-	public void testShouldBuildSecondDefaultCommandAndArgumentsWhenNoJregrReader() throws Exception {
-		defaultDecoder.advance();
-		assertTrue(Arrays.equals(DEFAULT_SECOND_COMMAND_AND_ARGUMENTS, defaultDecoder.buildCommandAndArguments(binDirectory, CASENAME)));
-	}
-	
-	@Test
-	public void testShouldGetDefaultInputForSecondLineWhenNoJregrReader() throws Exception {
-		defaultDecoder.advance();
-		assertEquals(CASENAME+DEFAULT_EXTENSION2, defaultDecoder.getStdin(CASENAME));
-	}
-	
-	@Test
-	public void testCanResetDefaultDecoderToReadFromBeginning() throws Exception {
-		defaultDecoder.advance();
-		defaultDecoder.reset();
-		assertTrue(Arrays.equals(DEFAULT_FIRST_COMMAND_AND_ARGUMENTS, defaultDecoder.buildCommandAndArguments(binDirectory, CASENAME)));
-	}	
 
 }
