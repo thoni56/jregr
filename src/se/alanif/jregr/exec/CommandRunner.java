20c3951fa742d7c1f2f643160f8e823851b7110c
Thomas Nilefalk <thomas@nilefalk.se>
2021-01-29 18:38:50 +0100

    Remove CommandRunner.run()

--- src/se/alanif/jregr/exec/CommandRunner.java
+++ src/se/alanif/jregr/exec/CommandRunner.java
@@ -26,23 +26,6 @@ public class CommandRunner {
 		this.processBuilder = processBuilder;
 	}
 
-	public String run(Process p, StreamGobbler errorGobbler, StreamGobbler outputGobbler, StreamPusher inputPusher) {
-		errorGobbler.start();
-		outputGobbler.start();
-
-		if (inputPusher != null) {
-			inputPusher.start();
-		}
-
-		try {
-			p.waitFor();
-			errorGobbler.join();
-			outputGobbler.join();
-		} catch (InterruptedException e) {
-		}
-		return outputGobbler.output() + errorGobbler.output();
-	}
-
 	public String runCommandForOutput(String[] commandAndArguments, String inputFilename) {
 		if (processBuilder == null) processBuilder = new ProcessBuilderSpy();
 
