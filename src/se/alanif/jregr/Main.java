8b27601daeadf7a8de0fc7a2233287b0a10ab899
Thomas Nilefalk <thomas@nilefalk.se>
2021-04-27 22:52:37 +0200

    Only print one XML header even if recursing

--- src/se/alanif/jregr/Main.java
+++ src/se/alanif/jregr/Main.java
@@ -14,124 +14,124 @@ import se.alanif.jregr.io.Directory;
 import se.alanif.jregr.reporters.RegrReporter;
 
 public class Main {
-	private static final String JREGR_VERSION = "0.2.2";
+    private static final String JREGR_VERSION = "0.2.3";
 
-	private void error(final String message) {
-		System.out.println("Error: " + message);
-	}
+    private void error(final String message) {
+        System.out.println("Error: " + message);
+    }
 
-	private void wrongDirectory(Directory directory, String reason) {
-		final String message = "Directory '" + directory.getName() + "' " + reason;
-		error(message);
-	}
+    private void wrongDirectory(Directory directory, String reason) {
+        final String message = "Directory '" + directory.getName() + "' " + reason;
+        error(message);
+    }
 
-	private Directory currentDirectory() {
-		return new Directory(System.getProperty("user.dir"));
-	}
+    private Directory currentDirectory() {
+        return new Directory(System.getProperty("user.dir"));
+    }
 
-	private Directory findRegressionDirectory(CommandLine commandLine) {
-		Directory directory;
-		if (commandLine.hasOption("dir")) {
-			String path = commandLine.getOptionValue("dir");
-			if (path.charAt(path.length() - 1) == '\\' || path.charAt(path.length() - 1) == '/')
-				path = path.substring(0, path.length()-1);
-			directory = new Directory(path);
-		} else {
-			directory = currentDirectory();
-		}
-		return directory;
-	}
+    private Directory findRegressionDirectory(CommandLine commandLine) {
+        Directory directory;
+        if (commandLine.hasOption("dir")) {
+            String path = commandLine.getOptionValue("dir");
+            if (path.charAt(path.length() - 1) == '\\' || path.charAt(path.length() - 1) == '/')
+                path = path.substring(0, path.length()-1);
+            directory = new Directory(path);
+        } else {
+            directory = currentDirectory();
+        }
+        return directory;
+    }
 
-	private Directory findBinDirectory(CommandLine commandLine) {
-		Directory binDirectory = null;
-		if (commandLine.hasOption("bin")) {
-			binDirectory = new Directory(commandLine.getOptionValue("bin"));
-		}
-		return binDirectory;
-	}
+    private Directory findBinDirectory(CommandLine commandLine) {
+        Directory binDirectory = null;
+        if (commandLine.hasOption("bin")) {
+            binDirectory = new Directory(commandLine.getOptionValue("bin"));
+        }
+        return binDirectory;
+    }
 
-	private boolean core(String[] args) {
-		OptionsManager optionManager = new OptionsManager();
-		HelpFormatter helpFormatter = new HelpFormatter();
-		boolean result = true;
-		try {
-			CommandLine commandLine = new DefaultParser().parse(optionManager, args);
+    private boolean core(String[] args) {
+        OptionsManager optionManager = new OptionsManager();
+        HelpFormatter helpFormatter = new HelpFormatter();
+        boolean result = true;
+        try {
+            CommandLine commandLine = new DefaultParser().parse(optionManager, args);
 
-			if (commandLine.hasOption("help")) {
-				helpFormatter.printHelp("jregr", optionManager);
-			} else if (commandLine.hasOption("version")) {
-				System.out.println("Jregr version " + JREGR_VERSION);
-			} else {
-				result = runCases(commandLine);
-			}
-		} catch (ParseException e) {
-			System.out.println("Argument error - " + e.getMessage());
-			helpFormatter.printHelp("jregr", optionManager);
-			result = false;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
+            if (commandLine.hasOption("help")) {
+                helpFormatter.printHelp("jregr", optionManager);
+            } else if (commandLine.hasOption("version")) {
+                System.out.println("Jregr version " + JREGR_VERSION);
+            } else {
+                result = runCases(commandLine);
+            }
+        } catch (ParseException e) {
+            System.out.println("Argument error - " + e.getMessage());
+            helpFormatter.printHelp("jregr", optionManager);
+            result = false;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 
-	private boolean runCases(CommandLine commandLine) throws IOException {
-		Directory directory = findRegressionDirectory(commandLine);
-		if (!directory.exists())
-			wrongDirectory(directory, "does not exist");
-		else
-			try {
-				final RegrDirectory regrDirectory = new RegrDirectory(directory);
-				final File commandsFile = regrDirectory.getCommandsFile();
-				if (commandsFile != null && commandsFile.length() > 0) {
-					final RegrCase[] cases = findSelectedCases(commandLine, regrDirectory);
+    private boolean runCases(CommandLine commandLine) throws IOException {
+        Directory directory = findRegressionDirectory(commandLine);
+        if (!directory.exists())
+            wrongDirectory(directory, "does not exist");
+        else
+            try {
+                final RegrDirectory regrDirectory = new RegrDirectory(directory);
+                final File commandsFile = regrDirectory.getCommandsFile();
+                if (commandsFile != null && commandsFile.length() > 0) {
+                    final RegrCase[] cases = findSelectedCases(commandLine, regrDirectory);
 
-					Directory binDirectory = findBinDirectory(commandLine);
-					if (binDirectory != null)
-						binDirectory = canonise(binDirectory);
-					
-					final String suiteName = regrDirectory.getName();
-					final RegrReporter reporter = RegrReporter.createReporter(commandLine, directory);
-					
-					boolean result;
-					reporter.start(commandLine);
-					if (cases.length == 0)
-						result = regrDirectory.runAllCases(reporter, binDirectory, suiteName, commandLine);
-					else
-						result = regrDirectory.runSelectedCases(cases, reporter, binDirectory, suiteName, commandLine);
-					reporter.end();
-					return result;
-				} else
-					wrongDirectory(regrDirectory.toDirectory(), "- top level directory must have a non-empty .jregr file");
-			} catch (CommandSyntaxException e) {
-				wrongDirectory(directory, "- syntax error in .jregr file");
-			}
-		return false;
-	}
+                    Directory binDirectory = findBinDirectory(commandLine);
+                    if (binDirectory != null)
+                        binDirectory = canonise(binDirectory);
 
-	private Directory canonise(Directory binDirectory) {
-		Directory canonical = null;
-		try {
-			canonical = new Directory(binDirectory.getCanonicalPath());
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-		return canonical;
-	}
+                    final String suiteName = regrDirectory.getName();
+                    final RegrReporter reporter = RegrReporter.createReporter(commandLine, directory);
 
-	private RegrCase[] findSelectedCases(CommandLine commandLine, RegrDirectory regrDirectory) {
-		final String[] arguments = commandLine.getArgs();
-		RegrCase[] cases;
-		if (arguments.length > 0) {
-			cases = regrDirectory.getCases(arguments);
-		} else {
-			cases = new RegrCase[0];
-		}
-		return cases;
-	}
+                    boolean result;
+                    reporter.start(commandLine);
+                    if (cases.length == 0)
+                        result = regrDirectory.runAllCases(reporter, binDirectory, suiteName, commandLine);
+                    else
+                        result = regrDirectory.runSelectedCases(cases, reporter, binDirectory, suiteName, commandLine);
+                    reporter.end();
+                    return result;
+                } else
+                    wrongDirectory(regrDirectory.toDirectory(), "- top level directory must have a non-empty .jregr file");
+            } catch (CommandSyntaxException e) {
+                wrongDirectory(directory, "- syntax error in .jregr file");
+            }
+        return false;
+    }
 
-	public static void main(String[] args) {
-		System.exit(new Main().core(args) ? 0 : 1);
-	}
+    private Directory canonise(Directory binDirectory) {
+        Directory canonical = null;
+        try {
+            canonical = new Directory(binDirectory.getCanonicalPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return canonical;
+    }
+
+    private RegrCase[] findSelectedCases(CommandLine commandLine, RegrDirectory regrDirectory) {
+        final String[] arguments = commandLine.getArgs();
+        RegrCase[] cases;
+        if (arguments.length > 0) {
+            cases = regrDirectory.getCases(arguments);
+        } else {
+            cases = new RegrCase[0];
+        }
+        return cases;
+    }
+
+    public static void main(String[] args) {
+        System.exit(new Main().core(args) ? 0 : 1);
+    }
 
 }
--- src/se/alanif/jregr/reporters/XMLReporter.java
+++ src/se/alanif/jregr/reporters/XMLReporter.java
@@ -19,148 +19,148 @@ import se.alanif.jregr.io.File;
 
 public class XMLReporter extends AbstractRegrReporter {
 
-	private static final String CONTROL_CHARACTER_REGEX = "[\\p{Cntrl}&&[^\r\n\t]]";
-	private String suiteName;
-	private RegrCase theCase;
-	private PrintStream xmlOutput = System.out;
-	private long millis;
+    private static final String CONTROL_CHARACTER_REGEX = "[\\p{Cntrl}&&[^\r\n\t]]";
+    private String suiteName;
+    private RegrCase theCase;
+    private PrintStream xmlOutput = System.out;
+    private long millis;
 
-	public XMLReporter(Directory regrDirectory) throws FileNotFoundException {
-		File xmlFile = regrDirectory.getFile("TEST-jregr.xml");
-		OutputStream xmlStream = new FileOutputStream(xmlFile);
-		xmlOutput = new PrintStream(xmlStream);
-	}
+    public XMLReporter(Directory regrDirectory) throws FileNotFoundException {
+        File xmlFile = regrDirectory.getFile("TEST-jregr.xml");
+        OutputStream xmlStream = new FileOutputStream(xmlFile);
+        xmlOutput = new PrintStream(xmlStream);
+    }
 
-	// No arguments constructor for tests
-	public XMLReporter() {
-	}
+    // No arguments constructor for tests
+    public XMLReporter() {
+    }
 
-	public void start(CommandLine commandLine) {
-	}
+    public void start(CommandLine commandLine) {
+        xmlOutput.println("<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>");
+    }
 
-	public void startSuite(String suiteName, int numberOfTests) {
-		this.suiteName = suiteName;
-		xmlOutput.println("<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>");
-		xmlOutput.println("<testsuite name=\"" + suiteName + "\">");
-	}
+    public void startSuite(String suiteName, int numberOfTests) {
+        this.suiteName = suiteName;
+        xmlOutput.println("<testsuite name=\"" + suiteName + "\">");
+    }
 
-	public void startTest(RegrCase caseName, long millis) {
-		this.theCase = caseName;
-		this.millis = millis;
-	}
+    public void startTest(RegrCase caseName, long millis) {
+        this.theCase = caseName;
+        this.millis = millis;
+    }
 
-	public void fatal() {
-		fatal++;
-		header(theCase.getName(), millis);
-		xmlOutput.println("    <error type=\"Fatal\" message=\"Case '" + theCase + "' failed to complete\">");
-		xmlOutput.println("    </error>");
-		tail();
-	}
+    public void fatal() {
+        fatal++;
+        header(theCase.getName(), millis);
+        xmlOutput.println("    <error type=\"Fatal\" message=\"Case '" + theCase + "' failed to complete\">");
+        xmlOutput.println("    </error>");
+        tail();
+    }
 
-	public void virgin() {
-		virgin++;
-		header(theCase.getName(), millis);
-		xmlOutput.println("    <error type=\"Virgin\" message=\"No input defined for case '" + theCase + "'\">");
-		xmlOutput.println("      The file '" + theCase + ".input' does not exist");
-		xmlOutput.println("    </error>");
-		tail();
-	}
+    public void virgin() {
+        virgin++;
+        header(theCase.getName(), millis);
+        xmlOutput.println("    <error type=\"Virgin\" message=\"No input defined for case '" + theCase + "'\">");
+        xmlOutput.println("      The file '" + theCase + ".input' does not exist");
+        xmlOutput.println("    </error>");
+        tail();
+    }
 
-	public void pending() {
-		pending++;
-		header(theCase.getName(), millis);
-		xmlOutput.println(
-				"    <error type=\"Pending\" message=\"No expected output defined for case '" + theCase + "'\">");
-		xmlOutput.println("      The file '" + theCase + ".expected' does not exist");
-		xmlOutput.println("    </error>");
-		tail();
-	}
+    public void pending() {
+        pending++;
+        header(theCase.getName(), millis);
+        xmlOutput.println(
+                "    <error type=\"Pending\" message=\"No expected output defined for case '" + theCase + "'\">");
+        xmlOutput.println("      The file '" + theCase + ".expected' does not exist");
+        xmlOutput.println("    </error>");
+        tail();
+    }
 
-	public void fail() {
-		failing++;
-		header(theCase.getName(), millis);
-		xmlOutput.println("    <failure message=\"Output does not match expected\">");
-		insertDiff(theCase, xmlOutput);
-		xmlOutput.println("    </failure>");
-		tail();
-	}
+    public void fail() {
+        failing++;
+        header(theCase.getName(), millis);
+        xmlOutput.println("    <failure message=\"Output does not match expected\">");
+        insertDiff(theCase, xmlOutput);
+        xmlOutput.println("    </failure>");
+        tail();
+    }
 
-	private static String xml10pattern = "[^" + "\u0009\r\n" + "\u0020-\uD7FF" + "\uE000-\uFFFD"
-			+ "\ud800\udc00-\udbff\udfff" + "]";
+    private static String xml10pattern = "[^" + "\u0009\r\n" + "\u0020-\uD7FF" + "\uE000-\uFFFD"
+            + "\ud800\udc00-\udbff\udfff" + "]";
 
-	private void insertDiff(RegrCase theCase, PrintStream outputStream) {
-		xmlOutput.println("        <![CDATA[Compared to the expected output, the actual has");
-		File outputFile = theCase.getOutputFile();
-		String output = outputFile.getContent();
-		if (output.matches(CONTROL_CHARACTER_REGEX)) {
-			// Need to create a new file because Diff API is (File, File)
-			java.io.File tempFile = null;
-			try {
-				output = output.replaceAll(xml10pattern, "");
-				tempFile = File.createTempFile("jregr", "output");
-			} catch (IOException e) {
-				xmlOutput.println("FATAL - Could not create temporary file");
-			}
-			new Diff(xmlOutput).doDiff(theCase.getExpectedFile(), tempFile);
-		} else
-			new Diff(xmlOutput).doDiff(theCase.getExpectedFile(), theCase.getOutputFile());
-		outputStream.println("]]>");
-	}
+    private void insertDiff(RegrCase theCase, PrintStream outputStream) {
+        xmlOutput.println("        <![CDATA[Compared to the expected output, the actual has");
+        File outputFile = theCase.getOutputFile();
+        String output = outputFile.getContent();
+        if (output.matches(CONTROL_CHARACTER_REGEX)) {
+            // Need to create a new file because Diff API is (File, File)
+            java.io.File tempFile = null;
+            try {
+                output = output.replaceAll(xml10pattern, "");
+                tempFile = File.createTempFile("jregr", "output");
+            } catch (IOException e) {
+                xmlOutput.println("FATAL - Could not create temporary file");
+            }
+            new Diff(xmlOutput).doDiff(theCase.getExpectedFile(), tempFile);
+        } else
+            new Diff(xmlOutput).doDiff(theCase.getExpectedFile(), theCase.getOutputFile());
+        outputStream.println("]]>");
+    }
 
-	public void pass() {
-		passing++;
-		header(theCase.getName(), millis);
-		insertExpectedOutput(theCase, xmlOutput);
-		tail();
-	}
+    public void pass() {
+        passing++;
+        header(theCase.getName(), millis);
+        insertExpectedOutput(theCase, xmlOutput);
+        tail();
+    }
 
-	private void insertExpectedOutput(RegrCase theCase, PrintStream outputStream) {
-		outputStream.println("        <![CDATA[");
-		try {
-			BufferedReader expectedReader = new BufferedReader(new FileReader(theCase.getExpectedFile()));
-			String line = "";
-			while ((line = expectedReader.readLine()) != null)
-				outputStream.println(line);
-			expectedReader.close();
-		} catch (FileNotFoundException e) {
-		} catch (IOException e) {
-		}
-		outputStream.println("]]>");
-	}
+    private void insertExpectedOutput(RegrCase theCase, PrintStream outputStream) {
+        outputStream.println("        <![CDATA[");
+        try {
+            BufferedReader expectedReader = new BufferedReader(new FileReader(theCase.getExpectedFile()));
+            String line = "";
+            while ((line = expectedReader.readLine()) != null)
+                outputStream.println(line);
+            expectedReader.close();
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        }
+        outputStream.println("]]>");
+    }
 
-	public void suspended() {
-		suspended++;
-		header(theCase.getName(), 0);
-		xmlOutput.println("    <skipped />");
-		tail();
-	}
+    public void suspended() {
+        suspended++;
+        header(theCase.getName(), 0);
+        xmlOutput.println("    <skipped />");
+        tail();
+    }
 
-	public void suspendedAndFailed() {
-		suspended();
-	}
+    public void suspendedAndFailed() {
+        suspended();
+    }
 
-	public void suspendedAndPassed() {
-		suspended();
-	}
+    public void suspendedAndPassed() {
+        suspended();
+    }
 
-	public void endSuite() {
-		xmlOutput.println("</testsuite>");
-	}
+    public void endSuite() {
+        xmlOutput.println("</testsuite>");
+    }
 
-	private void header(String caseName, long millis) {
-		xmlOutput.println("  <testcase classname=\"" + suiteName + "\" name=\"" + caseName + "\" time=\""
-				+ (float) millis / 1000 + "\">");
-	}
+    private void header(String caseName, long millis) {
+        xmlOutput.println("  <testcase classname=\"" + suiteName + "\" name=\"" + caseName + "\" time=\""
+                + (float) millis / 1000 + "\">");
+    }
 
-	private void tail() {
-		xmlOutput.println("  </testcase>");
-	}
+    private void tail() {
+        xmlOutput.println("  </testcase>");
+    }
 
-	public String removeControlCharactersFrom(String inputString) {
-		return inputString.replaceAll(CONTROL_CHARACTER_REGEX, "");
-	}
-	
-	public void end() {
-	}
+    public String removeControlCharactersFrom(String inputString) {
+        return inputString.replaceAll(CONTROL_CHARACTER_REGEX, "");
+    }
+
+    public void end() {
+    }
 
 }
