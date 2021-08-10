987f4e5a2e53103e71a2a24802bd7af489cd7e80
Thomas Nilefalk <thomas@nilefalk.se>
2021-08-10 23:24:45 +0200

    Remove `var` to be compatibe with Java 8

--- src/se/alanif/jregr/io/Directory.java
+++ src/se/alanif/jregr/io/Directory.java
@@ -10,61 +10,61 @@ import se.alanif.jregr.CommandDecoder;
 
 public class Directory extends File {
 
-	public Directory(String path) {
-		super(path);
-	}
+    public Directory(String path) {
+        super(path);
+    }
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public boolean executableExist(String name) {
-		return hasFile(name) || hasFile(name + ".exe");
-	}
+    public boolean executableExist(String name) {
+        return hasFile(name) || hasFile(name + ".exe");
+    }
 
-	public boolean hasFile(String fileName) {
-		return getFile(fileName).exists();
-	}
+    public boolean hasFile(String fileName) {
+        return getFile(fileName).exists();
+    }
 
-	public File getFile(String fileName) {
-		return new File(getAbsolutePath() + separator + fileName);
-	}
+    public File getFile(String fileName) {
+        return new File(getAbsolutePath() + separator + fileName);
+    }
 
-	public String[] getFilenamesWithExtension(String extension) {
-		return list(new FilenameFilter() {
+    public String[] getFilenamesWithExtension(String extension) {
+        return list(new FilenameFilter() {
 
-			@Override
-			public boolean accept(java.io.File dir, String name) {
-				return name.endsWith(extension);
-			}
-		});
-	}
+            @Override
+            public boolean accept(java.io.File dir, String name) {
+                return name.endsWith(extension);
+            }
+        });
+    }
 
-	public BufferedReader getBufferedReaderForFile(File file) {
-		if (file.exists())
-			try {
-				return new BufferedReader(new FileReader(file));
-			} catch (FileNotFoundException e) {
-				return null;
-			}
-		else
-			return null;
-	}
+    public BufferedReader getBufferedReaderForFile(File file) {
+        if (file.exists())
+            try {
+                return new BufferedReader(new FileReader(file));
+            } catch (FileNotFoundException e) {
+                return null;
+            }
+        else
+            return null;
+    }
 
-	private FileFilter subDirectoryFilter = new FileFilter() {
-		public boolean accept(java.io.File file) {
-			return file.isDirectory();
-		}
-	};
+    private FileFilter subDirectoryFilter = new FileFilter() {
+        public boolean accept(java.io.File file) {
+            return file.isDirectory();
+        }
+    };
 
-	public Directory[] getSubdirectories() {
-		java.io.File javaFiles[] = listFiles(subDirectoryFilter);
-		Directory subdirectories[] = new Directory[javaFiles.length];
-        for (var i = 0; i < javaFiles.length; i++)
+    public Directory[] getSubdirectories() {
+        java.io.File javaFiles[] = listFiles(subDirectoryFilter);
+        Directory subdirectories[] = new Directory[javaFiles.length];
+        for (int i = 0; i < javaFiles.length; i++)
             subdirectories[i] = new Directory(javaFiles[i].getPath());
-		return subdirectories;
-	}
+        return subdirectories;
+    }
 
-	public boolean exists(String filename) {
-		return new java.io.File(getPath(), filename).isFile();
-	}
+    public boolean exists(String filename) {
+        return new java.io.File(getPath(), filename).isFile();
+    }
 
 }
